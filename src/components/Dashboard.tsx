cc98801673e9fc6a5d73c6c8d4c9a1cbe9fc4d5f
hyperst8 <huypn82@gmail.com>
2024-10-27 21:02:58 +0100

    Add styles dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,31 @@
+import "@/styles/Dashboard.scss";
+import { Link } from "react-router-dom";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const locations = [
+    { id: "my-location", name: "My location", temp: 14 },
+    { id: "berlin", name: "Berlin", temp: 12 },
+    { id: "london", name: "London", temp: 10 },
+  ];
+
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+      <div className="location-list">
+        {locations.map((location) => (
+          <Link
+            key={location.id}
+            to={`/details/${location.id}`}
+          >
+            <div className="location-item">
+              <span>{location.name}</span>
+              <span>{location.temp}°C</span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,5 +1,14 @@
+import { useParams } from "react-router-dom";
+
 const Details = () => {
-  return <div>Details</div>;
+  const { locationId } = useParams();
+
+  return (
+    <div>
+      <h1>{locationId} Details</h1>
+      {/* Render detailed weather information for the selected location here */}
+    </div>
+  );
 };
 
 export default Details;
