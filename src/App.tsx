7a5a6fc50fac92c6b158a95502bb0d38f51577a0
hyperst8 <huypn82@gmail.com>
2024-10-27 16:07:57 +0100

    Initial setup with routing

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Dashboard from "@/components/Dashboard";
+import Details from "@/components/Details";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Dashboard />}
+        />
+        <Route
+          path="/details/:locationId"
+          element={<Details />}
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
