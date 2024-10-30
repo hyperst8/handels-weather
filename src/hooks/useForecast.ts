e3aa3cbcca33bb6a6c1b0df06774e6a348a7ea59
huy <huy@fagmann.no>
2024-10-30 15:56:26 +0100

    Slice forecast data

--- src/hooks/useForecast.ts
+++ src/hooks/useForecast.ts
@@ -1,5 +1,5 @@
 import { ChangeEvent, useEffect, useState } from "react";
-import { optionType } from "../types";
+import { optionType, forecastType } from "../types";
 
 const useForeCast = () => {
 	const [term, setTerm] = useState<string>("");
@@ -39,13 +39,19 @@ const useForeCast = () => {
 
 	const getForecast = (location: optionType) => {
 		fetch(
-			`https://api.openweathermap.org/data/2.5/weather?lat=${
+			`https://api.openweathermap.org/data/2.5/forecast?lat=${
 				location.lat
-			}&lon=${location.lon}&units=metric&appid=${import.meta.env.VITE_API_KEY}`
+			}&lon=${location.lon}&units=metric&lang=no&appid=${
+				import.meta.env.VITE_API_KEY
+			}`
 		)
 			.then((res) => res.json())
 			.then((data) => {
-				setForecast(data);
+				const forecastData = {
+					...data.city,
+					list: data.list.slice(0, 16),
+				};
+				setForecast(forecastData);
 			});
 	};
 
