396f32ee61258d99ad96ed157e3b04253049551d
huy <huy@fagmann.no>
2024-10-30 15:56:01 +0100

    define forecast type

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,41 @@ export type optionType = {
 	lat: number;
 	lon: number;
 };
+
+export type forecastType = {
+	name: string;
+	country: string;
+	sunset: number;
+	sunrise: number;
+	list: [
+		{
+			dt: number;
+			main: {
+				feels_like: number;
+				humidity: number;
+				pressure: number;
+				temp: number;
+				temp_min: number;
+				temp_max: number;
+			};
+			weather: [
+				{
+					id: number;
+					main: string;
+					description: string;
+					icon: string;
+				}
+			];
+			clouds: {
+				all: number;
+			};
+			wind: {
+				speed: number;
+				deg: number;
+				gust: number;
+			};
+			visibility: number;
+			pop: number;
+		}
+	];
+};
