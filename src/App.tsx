97fe1152f8a24e83c507b9d0e5f40dac8945146e
hyperst8 <huypn82@gmail.com>
2024-10-31 21:45:56 +0100

    change routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Details from "./components/Details";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 const App = (): JSX.Element => {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Dashboard />} />
-				<Route path="/details/:lat/:lon" element={<Details />} />
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Dashboard />}
+        />
+        <Route
+          path="/details/:name/:lat/:lon"
+          element={<Details />}
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
