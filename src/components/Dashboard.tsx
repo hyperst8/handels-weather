251fb47f40003767ca3ebcc352d2ea30d43b23bb
huy <huy@fagmann.no>
2024-11-03 09:18:26 +0100

    Change dummy locations

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,55 +5,54 @@ import useForecast from "../hooks/useForecast";
 import Search from "./helpers/Search";
 
 const Dashboard = (): JSX.Element => {
-  const {
-    term,
-    options,
-    onInputChange,
-    handleKeyDown,
-    handleOptionClick,
-    highlightedIndex,
-  } = useForecast();
+	const {
+		term,
+		options,
+		onInputChange,
+		handleKeyDown,
+		handleOptionClick,
+		highlightedIndex,
+	} = useForecast();
 
-  const locations = [
-    { id: "my-location", name: "My location", temp: 14 },
-    { id: "berlin", name: "Berlin", temp: 12 },
-    { id: "london", name: "London", temp: 10 },
-    { id: "paris", name: "Paris", temp: 11 },
-    { id: "tokyo", name: "Tokyo", temp: 15 },
-    { id: "new-york", name: "New York", temp: 13 },
-  ];
+	const locations = [
+		{ id: "berlin", name: "Berlin", temp: 12, lat: 52.52, lon: 13.41 },
+		{ id: "london", name: "London", temp: 10, lat: 51.51, lon: -0.13 },
+		{ id: "paris", name: "Paris", temp: 11, lat: 48.86, lon: 2.35 },
+		{ id: "tokyo", name: "Tokyo", temp: 15, lat: 35.69, lon: 139.69 },
+		{ id: "new-york", name: "New York", temp: 13, lat: 40.71, lon: -74.01 },
+	];
 
-  return (
-    <div className="dashboard">
-      <h1>Handelsbanken Weather</h1>
+	return (
+		<div className="dashboard">
+			<h1>Handelsbanken Weather</h1>
 
-      <Search
-        term={term}
-        options={options}
-        onInputChange={onInputChange}
-        handleKeyDown={handleKeyDown}
-        handleOptionClick={handleOptionClick}
-        highlightedIndex={highlightedIndex}
-      />
+			<Search
+				term={term}
+				options={options}
+				onInputChange={onInputChange}
+				handleKeyDown={handleKeyDown}
+				handleOptionClick={handleOptionClick}
+				highlightedIndex={highlightedIndex}
+			/>
 
-      <div className="locations-container">
-        <div className="location-list">
-          {locations.map((location) => (
-            <Link
-              className="link-btn"
-              key={location.id}
-              to={`/details/${location.id}`}
-            >
-              <div className="location-item">
-                <span>{location.name}</span>
-                <span>{location.temp}°C</span>
-              </div>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+			<div className="locations-container">
+				<div className="location-list">
+					{locations.map((location) => (
+						<Link
+							className="link-btn"
+							key={location.id}
+							to={`/details/${location.name}/${location.lat}/${location.lon}`}
+						>
+							<div className="location-item">
+								<span>{location.name}</span>
+								<span>{location.temp}°C</span>
+							</div>
+						</Link>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Dashboard;
