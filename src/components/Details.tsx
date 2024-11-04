c2545748dd0c9b1b19bbbb8771f562ac93df76f7
hyperst8 <huypn82@gmail.com>
2024-11-04 22:38:50 +0100

    Add id

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -7,140 +7,143 @@ import useForeCast from "../hooks/useForecast";
 import Degree from "./helpers/Degree";
 
 const Details = (): JSX.Element => {
-	const { name, lat, lon } = useParams<{
-		name: string;
-		lat: string;
-		lon: string;
-	}>();
-	const { getForecast, forecast, navigate, changeUnit, unit } = useForeCast();
+  const { name, lat, lon } = useParams<{
+    name: string;
+    lat: string;
+    lon: string;
+  }>();
+  const { getForecast, forecast, navigate, changeUnit, unit } = useForeCast();
 
-	useEffect(() => {
-		if (lat && lon) {
-			// Re-fetch forecast data when unit changes
-			getForecast({
-				lat: parseFloat(lat),
-				lon: parseFloat(lon),
-				name: name || "",
-				country: "",
-			});
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [name, lat, lon, unit]);
+  useEffect(() => {
+    if (lat && lon) {
+      // Re-fetch forecast data when unit changes
+      getForecast({
+        lat: parseFloat(lat),
+        lon: parseFloat(lon),
+        name: name || "",
+        country: "",
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [name, lat, lon, unit]);
 
-	if (!forecast) {
-		return <div className="loading">Loading...</div>;
-	}
+  if (!forecast) {
+    return <div className="loading">Loading...</div>;
+  }
 
-	// console.log(forecast);
-	const { country, sunrise, sunset, list } = forecast;
-	const today = list[0];
+  // console.log(forecast);
+  const { id, country, sunrise, sunset, list } = forecast;
+  const today = list[0];
 
-	return (
-		<div className="details">
-			<div className="details-header">
-				<button
-					className="back-btn"
-					aria-label="back button"
-					onClick={() => navigate("/")}
-				>
-					<IoArrowBackOutline aria-label="back arrow" />
-				</button>
-				<h1>
-					{forecast.name}, <span className="countryLabel">{country}</span>
-				</h1>
-			</div>
-			<div className="unit-buttons">
-				<button
-					className={`${unit === "metric" ? "active" : ""}`}
-					onClick={() => changeUnit("metric")}
-				>
-					°C
-				</button>
-				<button
-					className={`${unit === "imperial" ? "active" : ""}`}
-					onClick={() => changeUnit("imperial")}
-				>
-					°F
-				</button>
-			</div>
-			{/* Render detailed weather information for the selected location here */}
-			<div className="details-content">
-				{/* Render weather data for current day */}
-				<div className="weather-forecast">
-					<p className="weather-type">{today.weather[0].description}</p>
-					<img
-						src={`http://openweathermap.org/img/wn/${today.weather[0].icon}@2x.png`}
-						alt={`weather-icon-${today.weather[0].description}`}
-					/>
-					<p className="weather-degree">
-						{Math.round(today.main.temp)} <Degree unit={unit} />
-					</p>
-					<div className="degree-h-l">
-						<span className="h-l">
-							H: {Math.ceil(today.main.temp_max)} <Degree unit={unit} />
-						</span>
-						<span className="h-l">
-							L: {Math.floor(today.main.temp_min)} <Degree unit={unit} />
-						</span>
-					</div>
-					<div className="feels-like">
-						<span className="feels-like-label">Feels like: </span>
-						<span className="feels-like-value">
-							{Math.round(today.main.feels_like)} <Degree unit={unit} />
-						</span>
-						<br />
-						<span>
-							Feels{" "}
-							{Math.round(today.main.feels_like) < Math.round(today.main.temp)
-								? "colder"
-								: "warmer"}
-						</span>
-					</div>
-				</div>
-				{/* Render weather infon like sunrise, sunset, humidity, visibility */}
-				<div className="weather-info">
-					<div className="weather-info-item">
-						<span className="weather-info-label">Sunrise</span>
-						<span className="weather-info-value">{getSunTime(sunrise)}</span>
-					</div>
-					<div className="weather-info-item">
-						<span className="weather-info-label">Sunset</span>
-						<span className="weather-info-value">{getSunTime(sunset)}</span>
-					</div>
-					<div className="weather-info-item">
-						<span className="weather-info-label">Humidity</span>
-						<span className="weather-info-value">{today.main.humidity}%</span>
-					</div>
-					<div className="weather-info-item">
-						<span className="weather-info-label">Visibility</span>
-						<span className="weather-info-value">
-							{(today.visibility / 1000).toFixed()} km
-						</span>
-					</div>
-				</div>
-			</div>
-			{/* Render weather forecast for the next days */}
-			<div className="weather-forecast-list">
-				{list.map((item, index) => (
-					<div key={index} className="weather-forecast-item">
-						<p className="weather-forecast-day">
-							{index === 0 ? "Now" : formatHour(item.dt)}
-						</p>
-						<img
-							src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
-							alt={`weather-icon-${item.weather[0].description}`}
-						/>
-						<p className="weather-forecast-type">
-							{item.weather[0].description}
-						</p>
-						<p className="weather-forecast-degree">
-							{Math.round(item.main.temp)} <Degree unit={unit} />
-						</p>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="details">
+      <div className="details-header">
+        <button
+          className="back-btn"
+          aria-label="back button"
+          onClick={() => navigate("/")}
+        >
+          <IoArrowBackOutline aria-label="back arrow" />
+        </button>
+        <h1>
+          {forecast.name}, <span className="countryLabel">{country}</span>
+        </h1>
+      </div>
+      <div className="unit-buttons">
+        <button
+          className={`${unit === "metric" ? "active" : ""}`}
+          onClick={() => changeUnit("metric")}
+        >
+          °C
+        </button>
+        <button
+          className={`${unit === "imperial" ? "active" : ""}`}
+          onClick={() => changeUnit("imperial")}
+        >
+          °F
+        </button>
+      </div>
+      {/* Render detailed weather information for the selected location here */}
+      <div className="details-content">
+        {/* Render weather data for current day */}
+        <div className="weather-forecast">
+          <p className="weather-type">{today.weather[0].description}</p>
+          <img
+            src={`http://openweathermap.org/img/wn/${today.weather[0].icon}@2x.png`}
+            alt={`weather-icon-${today.weather[0].description}`}
+          />
+          <p className="weather-degree">
+            {Math.round(today.main.temp)} <Degree unit={unit} />
+          </p>
+          <div className="degree-h-l">
+            <span className="h-l">
+              H: {Math.ceil(today.main.temp_max)} <Degree unit={unit} />
+            </span>
+            <span className="h-l">
+              L: {Math.floor(today.main.temp_min)} <Degree unit={unit} />
+            </span>
+          </div>
+          <div className="feels-like">
+            <span className="feels-like-label">Feels like: </span>
+            <span className="feels-like-value">
+              {Math.round(today.main.feels_like)} <Degree unit={unit} />
+            </span>
+            <br />
+            <span>
+              Feels{" "}
+              {Math.round(today.main.feels_like) < Math.round(today.main.temp)
+                ? "colder"
+                : "warmer"}
+            </span>
+          </div>
+        </div>
+        {/* Render weather infon like sunrise, sunset, humidity, visibility */}
+        <div className="weather-info">
+          <div className="weather-info-item">
+            <span className="weather-info-label">Sunrise</span>
+            <span className="weather-info-value">{getSunTime(sunrise)}</span>
+          </div>
+          <div className="weather-info-item">
+            <span className="weather-info-label">Sunset</span>
+            <span className="weather-info-value">{getSunTime(sunset)}</span>
+          </div>
+          <div className="weather-info-item">
+            <span className="weather-info-label">Humidity</span>
+            <span className="weather-info-value">{today.main.humidity}%</span>
+          </div>
+          <div className="weather-info-item">
+            <span className="weather-info-label">Visibility</span>
+            <span className="weather-info-value">
+              {(today.visibility / 1000).toFixed()} km
+            </span>
+          </div>
+        </div>
+      </div>
+      {/* Render weather forecast for the next days */}
+      <div className="weather-forecast-list">
+        {list.map((item, index) => (
+          <div
+            key={index}
+            className="weather-forecast-item"
+          >
+            <p className="weather-forecast-day">
+              {index === 0 ? "Now" : formatHour(item.dt)}
+            </p>
+            <img
+              src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+              alt={`weather-icon-${item.weather[0].description}`}
+            />
+            <p className="weather-forecast-type">
+              {item.weather[0].description}
+            </p>
+            <p className="weather-forecast-degree">
+              {Math.round(item.main.temp)} <Degree unit={unit} />
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Details;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,53 +1,54 @@
 export type optionType = {
-	name: string;
-	country: string;
-	lat: number;
-	lon: number;
+  name: string;
+  country: string;
+  lat: number;
+  lon: number;
 };
 
 export type forecastType = {
-	name: string;
-	country: string;
-	sunset: number;
-	sunrise: number;
-	list: [
-		{
-			dt: number;
-			main: {
-				feels_like: number;
-				humidity: number;
-				pressure: number;
-				temp: number;
-				temp_min: number;
-				temp_max: number;
-			};
-			weather: [
-				{
-					id: number;
-					main: string;
-					description: string;
-					icon: string;
-				}
-			];
-			clouds: {
-				all: number;
-			};
-			wind: {
-				speed: number;
-				deg: number;
-				gust: number;
-			};
-			visibility: number;
-			pop: number;
-		}
-	];
+  id: number;
+  name: string;
+  country: string;
+  sunset: number;
+  sunrise: number;
+  list: [
+    {
+      dt: number;
+      main: {
+        feels_like: number;
+        humidity: number;
+        pressure: number;
+        temp: number;
+        temp_min: number;
+        temp_max: number;
+      };
+      weather: [
+        {
+          id: number;
+          main: string;
+          description: string;
+          icon: string;
+        }
+      ];
+      clouds: {
+        all: number;
+      };
+      wind: {
+        speed: number;
+        deg: number;
+        gust: number;
+      };
+      visibility: number;
+      pop: number;
+    }
+  ];
 };
 
 export type weatherType = {
-	name: string;
-	country: string;
-	temp: number;
-	lat: number;
-	lon: number;
-	id: number;
+  name: string;
+  country: string;
+  temp: number;
+  lat: number;
+  lon: number;
+  id: number;
 };
